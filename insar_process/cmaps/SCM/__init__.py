aa4595bef545fed57a8cbbaf6a7b7a54e3e36935
Fanchengyan <1431332678@qq.com>
2023-09-03 00:51:56 +0800

    fix bug of no _r attribute for SCM cmaps

--- insar_process/cmaps/SCM/__init__.py
+++ insar_process/cmaps/SCM/__init__.py
@@ -6,16 +6,20 @@ from matplotlib.colors import LinearSegmentedColormap
 
 cwd = Path(__file__).parent.absolute()
 
-__all__ = ['acton', 'bamako', 'batlow', 'berlin', 'bilbao', 'broc',
-           'brocO', 'buda', 'cork', 'corkO', 'davos', 'devon', 'grayC',
-           'hawaii', 'imola', 'lajolla', 'lapaz', 'lisbon', 'nuuk',
-           'oleron', 'oslo', 'roma', 'romaO', 'tofino', 'tokyo',
-           'turku', 'vik', 'vikO']
+names = ['acton', 'bamako', 'batlow', 'berlin', 'bilbao', 'broc',
+         'brocO', 'buda', 'cork', 'corkO', 'davos', 'devon', 'grayC',
+         'hawaii', 'imola', 'lajolla', 'lapaz', 'lisbon', 'nuuk',
+         'oleron', 'oslo', 'roma', 'romaO', 'tofino', 'tokyo',
+         'turku', 'vik', 'vikO']
 
+__all__ = names.copy()
 
-for name in __all__:
+for name in names:
     file = cwd / name / f'{name}.txt'
     cm_data = np.loadtxt(file)
+
+    __all__.append(f'{name}_r')
+
     vars()[name] = LinearSegmentedColormap.from_list(name, cm_data)
-    vars()["{name}_r"] = LinearSegmentedColormap.from_list(f"{name}_r", cm_data[::-1])
-    
+    vars()[f"{name}_r"] = LinearSegmentedColormap.from_list(
+        f"{name}_r", cm_data[::-1])
