7c4f6f0172a97522745990a2420f4045b079946b
Fanchengyan <1431332678@qq.com>
2023-09-08 01:13:51 +0800

    init sar_tools.py

---
+++ insar_process/utils/sar_tools.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def multi_look(
+    arr_in: np.ndarray,
+    azimuth_looks: int,
+    range_looks: int
+) -> np.ndarray:
+    '''Multi_look array with alks and rlks
+
+    Parameters
+    ----------
+    arr_in: numpy.ndarray
+        input array to be multi-looked
+    azimuth_looks: int
+        number of looks in azimuth
+    range_looks: int
+        number of looks in range
+    '''
+    pass
