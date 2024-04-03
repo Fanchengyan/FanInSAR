16f18348ec39f7afd99efd2eb808db9bda68160c
fanchengyan <fanchy14@lzu.edu.cn>
2024-04-03 15:00:56 +0800

    python_requires: 3.9->3.8

--- setup.py
+++ setup.py
@@ -50,5 +50,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.9',
+    python_requires='>=3.8',
 )
