fbb126fae2d44c06d29ff8b5ad980b3e95737ac6
Fanchengyan <1431332678@qq.com>
2024-06-23 21:12:47 +0800

    remove init import NSBAS, cmaps, constants, datasets, samplers

--- faninsar/__init__.py
+++ faninsar/__init__.py
@@ -1,7 +1,4 @@
-# from .utils import data, geo_tools
-from faninsar.NSBAS import *
-
-from . import NSBAS, cmaps, constants, datasets, samplers
+# from . import NSBAS, cmaps, constants, datasets, samplers
 from ._core import *
 
 __version__ = "0.0.1"
