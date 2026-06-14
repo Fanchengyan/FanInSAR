987a933adaff3073c5c1893e5a9deb1a40f5180a
Fanchengyan <1431332678@qq.com>
2026-06-14 19:18:14 +0800

    refactor: remove isce2 and isce3 processing modules

--- faninsar/isce2/executors/__init__.py
+++
@@ -1,8 +0,0 @@
-"""ISCE2 command executors.
-
-This module provides command execution functionality for ISCE2 processing.
-"""
-
-from .execute import execute_command
-
-__all__ = ["execute_command"]
--- faninsar/isce2/sensors/__init__.py
+++
@@ -1,9 +0,0 @@
-"""ISCE2 sensor-specific implementations.
-
-This module provides sensor abstraction for different SAR satellites.
-"""
-
-from faninsar.isce2.sensors.base import BaseSensor
-from faninsar.isce2.sensors.sentinel1 import Sentinel1Sensor
-
-__all__ = ["BaseSensor", "Sentinel1Sensor"]
